08bafff187e3db525f39853441716807de343243
MujtabaMamoun <44872348+MujtabaMamoun@users.noreply.github.com>
2018-11-12 08:23:55 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,17 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
-
-client.on("ready", () => {
-console.log('By : m7md');
-client.user.setPresence({
-  status: 'dnd',
-  game: { 
-     type: 0,
-     name: 'هاي يوتيوووب',
-     details: `هاي يوتيوب`,
-     url: 'http://twitch.tv/M7md_Salih',
-     state: `هاي يوتيوب`,
-    application_id: '377479790195769345',
-     assets: {
-        small_image: `377480550207717376`,
-        small_text: 'هاي يوتيوب',
-        large_image: `377480353259978752`,
-        large_text: `هاي يوتيوب` }
-
-  }
-    });
+client.on('ready', () => {
+  console.log(Logged in as ${client.user.tag}!);
 });
-اقولها
 
-client.login("MzU3MjkwMjYxNDA5NDMxNTUy.DV4gXw.uUb0cO18ipmgbmsgb-6niE39vBs");
+
+client.on('ready', async() => {
+var server = "511576651436523520"; // ايدي السررفر
+var channel = "511576651436523522";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , ')
+    },305);
+})
+
+client.login(NTAyODk4MTU1ODMxMTY0OTU3.Dsna5A.sHhOAmljfHudIULCl18tlg5wIL4);
